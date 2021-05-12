da1a25908ca5bc2c4c0379fbcaddc78c94d77517
suryapranesh_300068342 <surya.pranesh@myntra.com>
2021-05-12 13:38:02 +0530

    PORT env capitalised

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const typeDefs = require('./graphql/typeDef');
 const resolvers = require('./graphql/resolvers');
 
 const pubsub = new PubSub();
-const PORT = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 
 const server = new ApolloServer({ typeDefs, resolvers, context: ({ req }) => ({ req, pubsub }) });
 
